dba17b4652297cc2b2661b81df52110b707bdd2f
yadj2959 <yadj2959@rediffmail.com>
2020-05-15 01:03:05 +0530

    Now rolling two dice

--- intermediate-python-course/dice_roller.py
+++ intermediate-python-course/dice_roller.py
@@ -1,5 +1,12 @@
+import random
 def main():
-  print("You've rolled a die")
+  dice_rolls = 2
+  dice_sum = 0
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum = dice_sum + roll
+    print(f"You've rolled a {roll}")
+  print(f"You've rolled a total of {dice_sum}")
 
 if __name__== "__main__":
   main()
